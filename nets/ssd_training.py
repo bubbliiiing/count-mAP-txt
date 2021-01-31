454425ea9a5a8b7166e55c41722ea93b0684a002
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2021-01-31 23:46:53 +0800

    Update ssd_training.py

--- nets/ssd_training.py
+++ nets/ssd_training.py
@@ -70,14 +70,17 @@ class MultiboxLoss(object):
         # 找到了哪些值是大于0的
         pos_num_neg_mask = tf.greater(num_neg, 0)
         # --------------------------------------------- #
-        #   如果有些图，它的正样本数量为0，
-        #   默认负样本为100
+        #   如果所有的图，正样本的数量均为0
+        #   那么则默认选取100个先验框作为负样本
         # --------------------------------------------- #
         has_min = tf.to_float(tf.reduce_any(pos_num_neg_mask))
         num_neg = tf.concat(axis=0, values=[num_neg, [(1 - has_min) * self.negatives_for_hard]])
         
         # --------------------------------------------- #
-        #   求平均每个图片要取多少个负样本
+        #   从这里往后，与视频中看到的代码有些许不同。
+        #   由于以前的负样本选取方式存在一些问题，
+        #   我对该部分代码进行重构。
+        #   求整个batch应该的负样本数量总和
         # --------------------------------------------- #
         num_neg_batch = tf.reduce_sum(tf.boolean_mask(num_neg, tf.greater(num_neg, 0)))
         num_neg_batch = tf.to_int32(num_neg_batch)
@@ -92,18 +95,20 @@ class MultiboxLoss(object):
 
         # --------------------------------------------- #
         #   batch_size,8732
+        #   把不是背景的概率求和，求和后的概率越大
+        #   代表越难分类。
         # --------------------------------------------- #
         max_confs = tf.reduce_sum(y_pred[:, :, confs_start:confs_end], axis=2)
 
-        # --------------------------------------------- #
+        # --------------------------------------------------- #
         #   只有没有包含物体的先验框才得到保留
-        #   选取最难分类的负样本
-        # --------------------------------------------- #
+        #   我们在整个batch里面选取最难分类的num_neg_batch个
+        #   先验框作为负样本。
+        # --------------------------------------------------- #
         max_confs = tf.reshape(max_confs * (1 - y_true[:, :, -8]), [-1])
         _, indices = tf.nn.top_k(max_confs, k=num_neg_batch)
 
         neg_conf_loss = tf.gather(tf.reshape(conf_loss, [-1]), indices)
-        neg_conf_loss = tf.Print(neg_conf_loss, [tf.gather(tf.reshape(max_confs, [-1]), indices), neg_conf_loss], summarize=50)
 
         # 进行归一化
         num_pos     = tf.where(tf.not_equal(num_pos, 0), num_pos, tf.ones_like(num_pos))
@@ -113,6 +118,7 @@ class MultiboxLoss(object):
 
 def rand(a=0, b=1):
     return np.random.rand()*(b-a) + a
+
 
 class Generator(object):
     def __init__(self, bbox_util,batch_size,
@@ -258,4 +264,3 @@ class Generator(object):
                     inputs = []
                     targets = []
                     yield preprocess_input(tmp_inp), tmp_targets
-
